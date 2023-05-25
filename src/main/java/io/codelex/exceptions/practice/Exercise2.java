9e922921013bbeb5187506bd67bc95a3f7681598
IevaG <ieva.ivanane@gmail.com>
2023-05-25 20:08:58 +0300

    Exceptions

--- src/main/java/io/codelex/exceptions/practice/Exercise2.java
+++ src/main/java/io/codelex/exceptions/practice/Exercise2.java
@@ -1,12 +1,38 @@
 package io.codelex.exceptions.practice;
 
 public class Exercise2 {
+    public static void main(String[] args) {
+        Exercise2 ex = new Exercise2();
+        ex.methodA();
+    }
+
     public void methodA() throws ArithmeticException {
+        try {
+            methodB();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
     public void methodB() throws ArithmeticException {
+        try {
+            methodC();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
     public void methodC() throws ArithmeticException {
+        int x = 2;
+        int y = 0;
+        try {
+            int z = x / y;
+            System.out.println(z);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
