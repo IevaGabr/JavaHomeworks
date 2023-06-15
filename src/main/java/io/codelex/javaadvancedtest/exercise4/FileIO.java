9d1b6977d80f16277dbad5e6a8c6b6b0665cab1c
IevaG <ieva.ivanane@gmail.com>
2023-06-15 09:48:31 +0300

    Exercise4

--- src/main/java/io/codelex/javaadvancedtest/exercise4/FileIO.java
+++ src/main/java/io/codelex/javaadvancedtest/exercise4/FileIO.java
@@ -1,19 +1,36 @@
 package io.codelex.javaadvancedtest.exercise4;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class FileIO {
     public static void main(String[] args) throws IOException {
-        FileReader in = new FileReader("C:\\Codelex_Java\\JavaHomeworks\\src\\main\\resources\\javaadvancedtest\\exercise4\\FileToRead.txt");
-        FileWriter out = new FileWriter("C:\\Codelex_Java\\JavaHomeworks\\src\\main\\resources\\javaadvancedtest\\exercise4\\FileToWrite.txt");
-        int nextChar;
-        while ((nextChar = in.read()) != -1) {
-
+        String pathToApp = "src\\main\\resources\\javaadvancedtest\\exercise4\\";
+        String fileNameToRead = "FileToRead";
+        Path pathToRead = Paths.get(pathToApp + fileNameToRead + ".txt");
+        String fileNameToWrite = "";
+        String[] fileNameToReadArray = fileNameToRead.split("");
+        for (int i = fileNameToReadArray.length - 1; i >= 0; i--) {
+            fileNameToWrite = fileNameToWrite + fileNameToReadArray[i];
         }
-        in.close();
-        out.close();
+        Path pathToWrite = Paths.get(pathToApp + fileNameToWrite + ".txt");
+        Files.createFile(pathToWrite);
+        List<String> linesToRead = Files.readAllLines(pathToRead);
+        for (int i = linesToRead.size() - 1; i >= 0; i--) {
+            String[] lineArray = linesToRead.get(i).split("");
+            String lineToWrite = "";
+            for (int j = lineArray.length - 1; j >= 0; j--) {
+                lineToWrite = lineToWrite + lineArray[j];
+            }
+            if (i != 0) {
+                lineToWrite = lineToWrite + "\n";
+            }
+            Files.write(pathToWrite, lineToWrite.getBytes(), StandardOpenOption.APPEND);
+        }
+
     }
 }
